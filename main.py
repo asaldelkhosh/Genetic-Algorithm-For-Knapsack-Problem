ad2f97eace8db080d3288587c375edcd1a2912c7
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-19 12:12:40 +0430

    feat: initial population

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('vim')
+import random
+
+
+
+def generate_initial_population(count=6) -> List[Individual]:
+    population = set()
+
+    # generate initial population having `count` individuals
+    while len(population) != count:
+        # pick random bits one for each item and 
+        # create an individual 
+        bits = [
+            random.choice([0, 1])
+            for _ in items
+        ]
+        population.add(Individual(bits))
+
+    return list(population)
