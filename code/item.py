ee039d39434e5dbebc3279ec1f36cf5fc938c640
asaldelkhosh <asaldelkhosh@gmail.com>
2022-07-30 08:24:01 +0430

    add: item module

---
+++ code/item.py
@@ -0,0 +1,5 @@
+class Item:
+    def __init__(self, name, weight, value):
+        self.name = name
+        self.weight = weight
+        self.value = value
--- code/main.py
+++ code/main.py
@@ -1,13 +1,7 @@
 import random
 from typing import List
+from item import Item
 
-
-
-class Item:
-    def __init__(self, name, weight, value):
-        self.name = name
-        self.weight = weight
-        self.value = value
 
 
 class Individual:
